480cdffa3309473bf5815f9039a4d93b6ba465ca
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-19 20:25:14 +0600

    comment out unused route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,38 +1,38 @@
 import { Router } from 'express';
-import { AuthRoutes } from '../modules/Auth/auth.route';
-import { CarRoutes } from '../modules/Course/car.route';
-import { orderRoutes } from '../modules/Order/order.routes';
-import { paymentRoutes } from '../modules/Payment/payment.routes';
-import { ReviewRoutes } from '../modules/Review/review.route';
+// import { AuthRoutes } from '../modules/Auth/auth.route';
+// import { CarRoutes } from '../modules/Course/car.route';
+// import { orderRoutes } from '../modules/Order/order.routes';
+// import { paymentRoutes } from '../modules/Payment/payment.routes';
+// import { ReviewRoutes } from '../modules/Review/review.route';
 import { UserRoutes } from '../modules/User/user.route';
 
 const router = Router();
 
 const moduleRoutes = [
-  {
-    path: '/auth',
-    route: AuthRoutes,
-  },
-  {
-    path: '/cars',
-    route: CarRoutes,
-  },
-  {
-    path: '/reviews',
-    route: ReviewRoutes,
-  },
+  // {
+  //   path: '/auth',
+  //   route: AuthRoutes,
+  // },
+  // {
+  //   path: '/cars',
+  //   route: CarRoutes,
+  // },
+  // {
+  //   path: '/reviews',
+  //   route: ReviewRoutes,
+  // },
   {
     path: '/users',
     route: UserRoutes,
   },
-  {
-    path: '/orders',
-    route: orderRoutes,
-  },
-  {
-    path: '/payment',
-    route: paymentRoutes,
-  },
+  // {
+  //   path: '/orders',
+  //   route: orderRoutes,
+  // },
+  // {
+  //   path: '/payment',
+  //   route: paymentRoutes,
+  // },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
