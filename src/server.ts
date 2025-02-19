db3efe40973844fd705704f7aa9859f602f7fc60
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-19 12:11:55 +0600

    add server file

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
+
+let server: Server;
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    server = app.listen(config.port, () => {
+      console.log(`App listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log('Connection Error: ', error);
+  }
+}
+
+main();
+
+process.on('unhandledRejection', () => {
+  console.log(`😈 unhandledRejection is detected , shutting down ...`);
+
+  if (server) {
+    server.close();
+    process.exit(1);
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
