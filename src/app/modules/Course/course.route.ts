b5ffe5bc4baf8cbeb2df53293f7da8ccebeae4f8
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-19 21:21:23 +0600

    Add course route

---
+++ src/app/modules/Course/course.route.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { USER_ROLE } from '../User/user.constant';
+import { CourseController } from './course.controller';
+import { CourseValidation } from './course.validation';
+
+const router = express.Router();
+
+router.post(
+  '/',
+  auth(USER_ROLE.admin),
+  validateRequest(CourseValidation.createCourseValidationSchema),
+  CourseController.createCourse,
+);
+
+router.get('/', CourseController.getAllCourse);
+
+router.get('/:id', CourseController.getSingleCourse);
+
+router.put(
+  '/:id',
+  auth(USER_ROLE.admin),
+  validateRequest(CourseValidation.updateCourseValidationSchema),
+  CourseController.updateCourse,
+);
+
+router.delete('/:id', auth(USER_ROLE.admin), CourseController.deleteCourse);
+
+export const CourseRoutes = router;
