75a4b4bab3d0ffb5a1b1803cee9fd71c8b9b73b2
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-19 21:25:03 +0600

    add course service

---
+++ src/app/modules/Course/course.service.ts
@@ -0,0 +1,69 @@
+import httpStatus from 'http-status';
+import QueryBuilder from '../../builder/QueryBuilder';
+import AppError from '../../errors/AppError';
+import { TCourse } from './course.interface';
+import { Course } from './course.model';
+
+const createCourse = async (payload: TCourse) => {
+  const result = await Course.create(payload);
+  return result;
+};
+
+const getAllCourse = async (query: Record<string, unknown>) => {
+  const courseQuery = new QueryBuilder(Course.find(), query)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const result = await courseQuery.modelQuery;
+  return result;
+};
+
+const getSingleCourse = async (id: string) => {
+  const result = await Course.findById(id);
+
+  if (!result) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Course not found');
+  }
+
+  return result;
+};
+
+const updateCourse = async (id: string, payload: Partial<TCourse>) => {
+  const isCourseExists = await Course.findById(id);
+
+  if (!isCourseExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Course not found');
+  }
+
+  const result = await Course.findByIdAndUpdate(id, payload, {
+    new: true,
+  });
+
+  return result;
+};
+
+const deleteCourse = async (id: string) => {
+  const isCourseExists = await Course.findById(id);
+
+  if (!isCourseExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Course not found');
+  }
+
+  const result = await Course.findByIdAndUpdate(
+    id,
+    { isDeleted: true },
+    { new: true },
+  );
+
+  return result;
+};
+
+export const CourseService = {
+  createCourse,
+  getAllCourse,
+  getSingleCourse,
+  updateCourse,
+  deleteCourse,
+};
