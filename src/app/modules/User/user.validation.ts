13852426bd3b6cc5821de7b2a69e75d4cfc361e0
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-20 16:03:05 +0600

    add user validation

---
+++ src/app/modules/User/user.validation.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+const userValidationSchema = z.object({
+  body: z.object({
+    name: z.string({ required_error: 'Name is required' }),
+    email: z.string({ required_error: 'Email is required' }).email(),
+  }),
+});
+
+const userUpdateValidationSchema = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    email: z.string().email().optional(),
+  }),
+});
+
+export const UserValidation = {
+  userValidationSchema,
+  userUpdateValidationSchema,
+};
