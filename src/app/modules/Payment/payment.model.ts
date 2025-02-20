2255dc42ce16622dcc52a11e34507b688969f7f5
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-20 21:22:25 +0600

    add payment model

---
+++ src/app/modules/Payment/payment.model.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+import { TPayment } from './payment.interface';
+
+const paymentSchema = new Schema<TPayment>(
+  {
+    price: {
+      type: String,
+      required: true,
+    },
+    transactionId: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    courseId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Course',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Payment = model<TPayment>('Payment', paymentSchema);
