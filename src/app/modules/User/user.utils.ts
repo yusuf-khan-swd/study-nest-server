c90b4fad6e8db112aee5c70587acfb53acf4ea0c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-20 20:24:41 +0600

    add createToken utils

---
+++ src/app/modules/User/user.utils.ts
@@ -0,0 +1,9 @@
+import jwt from 'jsonwebtoken';
+
+export const createToken = (
+  jwtPayload: { email: string; role: string },
+  secret: string,
+  expiresIn: string,
+) => {
+  return jwt.sign(jwtPayload, secret, { expiresIn });
+};
