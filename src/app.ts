ae22e0de7b7aaf2daeb9edb33f1aeba87be52bbb
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-20 22:06:37 +0600

    add /create-payment-intent route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import cors from 'cors';
 import express from 'express';
 import path from 'path';
+import Stripe from 'stripe';
+import config from './app/config';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
 import router from './app/routes';
+
+const stripe = new Stripe(config.stripe_secret as string);
 
 const app = express();
 
@@ -16,6 +20,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Application route
 app.use('/api', router);
+
+app.post('/create-payment-intent', async (req, res) => {
+  const price = req.body.productPrice;
+  const amount = price * 100;
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: 'usd',
+    payment_method_types: ['card'],
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
