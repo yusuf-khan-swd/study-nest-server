2dfb1591ca8cdfba5fff2440d5666be1d3b6d97f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-20 11:01:53 +0600

    add todo for course

--- src/app/modules/Course/course.model.ts
+++ src/app/modules/Course/course.model.ts
@@ -6,7 +6,7 @@ const courseSchema = new Schema<TCourse>(
     title: { type: String, required: true },
     duration: { type: String, required: true },
     instructor: { type: String, required: true },
-    price: { type: String, required: true },
+    price: { type: String, required: true }, //TODO: make price number field and update interface, and validation field
     description: { type: String, required: true },
     email: { type: String, required: true },
   },
