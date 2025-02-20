d4c6bc9f76738744395d4c87c11ac006161b94fd
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-20 16:20:53 +0600

    add user model

---
+++ src/app/modules/User/user.model.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+import { TUser } from './user.interface';
+
+const userSchema = new Schema<TUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = model<TUser>('User', userSchema);
