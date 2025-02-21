e246f4055e67d1af465bb877833e5cfb4e8df867
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-21 17:33:21 +0600

    add a note for post route of createEnroll

--- src/app/modules/Enroll/enroll.route.ts
+++ src/app/modules/Enroll/enroll.route.ts
@@ -7,6 +7,7 @@ import { EnrollValidation } from './enroll.validation';
 
 const router = express.Router();
 
+// ! I don't think post route use in client
 router.post(
   '/',
   auth(USER_ROLE.user),
