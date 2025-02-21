a3b75ea89b6f3a47b9f4ed92f99c105448c6b483
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-21 21:33:03 +0600

    make name field for user validation optional

--- src/app/modules/User/user.validation.ts
+++ src/app/modules/User/user.validation.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 const userValidationSchema = z.object({
   body: z.object({
-    name: z.string({ required_error: 'Name is required' }),
+    name: z.string({ required_error: 'Name is required' }).optional(),
     email: z.string({ required_error: 'Email is required' }).email(),
   }),
 });
