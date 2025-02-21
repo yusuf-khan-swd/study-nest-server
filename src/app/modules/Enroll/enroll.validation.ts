5cd9fac9a30d426161594fd448fd6d39ce26c82c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-21 17:14:48 +0600

    Add enroll validation

--- src/app/modules/Enroll/enroll.validation.ts
+++ src/app/modules/Enroll/enroll.validation.ts
@@ -1,28 +1,13 @@
 import { z } from 'zod';
 
-const createCourseValidationSchema = z.object({
+const createEnrollValidationSchema = z.object({
   body: z.object({
-    title: z.string().min(1),
-    duration: z.string().min(1),
-    instructor: z.string().min(1),
-    price: z.string().min(1),
-    description: z.string().min(1),
     email: z.string().email(),
-  }),
-});
-
-const updateCourseValidationSchema = z.object({
-  body: z.object({
-    title: z.string().min(1).optional(),
-    duration: z.string().min(1).optional(),
-    instructor: z.string().min(1).optional(),
-    price: z.string().min(1).optional(),
-    description: z.string().min(1).optional(),
-    email: z.string().email().optional(),
+    payment: z.string().min(1),
+    course: z.string().min(1),
   }),
 });
 
 export const EnrollValidation = {
-  createCourseValidationSchema,
-  updateCourseValidationSchema,
+  createEnrollValidationSchema,
 };
