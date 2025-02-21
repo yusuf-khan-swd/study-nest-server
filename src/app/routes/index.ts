4f0cc14cb8d6d697bb67b1cdfad3a81786fa17f5
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-21 17:42:31 +0600

    remove commented import

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,9 +1,4 @@
 import { Router } from 'express';
-// import { AuthRoutes } from '../modules/Auth/auth.route';
-// import { CarRoutes } from '../modules/Course/car.route';
-// import { orderRoutes } from '../modules/Order/order.routes';
-// import { paymentRoutes } from '../modules/Payment/payment.routes';
-// import { ReviewRoutes } from '../modules/Review/review.route';
 import { CourseRoutes } from '../modules/Course/course.route';
 import { EnrollRoutes } from '../modules/Enroll/enroll.route';
 import { PaymentRoutes } from '../modules/Payment/payment.route';
