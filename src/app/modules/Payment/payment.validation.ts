9c6b3c4117af69c4041a4302ac30e4619c07e6e7
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-21 17:09:16 +0600

    Modify payment validation course property

--- src/app/modules/Payment/payment.validation.ts
+++ src/app/modules/Payment/payment.validation.ts
@@ -5,7 +5,7 @@ const paymentValidationSchema = z.object({
     price: z.string().min(1),
     transactionId: z.string().min(1),
     email: z.string().email(),
-    courseId: z.string().min(1),
+    course: z.string().min(1),
   }),
 });
 
