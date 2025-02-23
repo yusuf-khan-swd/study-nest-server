f42354e8de97d386e8d574da3972bc3ee3b67673
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-02-23 11:33:31 +0600

    fixed createToken jwt.sign function error by setting expiresIn: any

--- src/app/modules/User/user.utils.ts
+++ src/app/modules/User/user.utils.ts
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 export const createToken = (
   jwtPayload: { email: string; role: string; id: string },
   secret: string,
-  expiresIn: string,
+  expiresIn: any,
 ) => {
   return jwt.sign(jwtPayload, secret, { expiresIn });
 };
